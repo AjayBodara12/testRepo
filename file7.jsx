4909ab86c3cb9aa039c4c406eb1c88e12b57229a
AjayBodara12 <bodaraajay00@gmail.com>
2023-05-08 14:42:12 +0530

    added files demo.

---
+++ file7.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function file7() {
+  return (
+    <div>file7</div>
+  )
+}
---
+++ file8.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function file8() {
+  return (
+    <div>file8</div>
+  )
+}
